335df4e06dd1b00a035acb660c9c2d9a1bdbbab7
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2024-09-21 17:13:14 +0900

    feat: 서로 다른 작업을 수행하는 여러 스레드 생성

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h> // 프로세스 관련 함수 사용을 위해 포함(Linux 및 MacOS 에서만 지원되는 헤더)
@@ -7,12 +8,28 @@
   주! pthread_create() 에 넘겨줄 함수는 반드시 void* 타입의 매개변수 하나를
   정의해야 함.
  */
+// void *foo(void *arg) {
+//   // 현재 실행 중인 thread 의 식별자(스레드 ID) 반환
+//   // -> std::thread 로 치면 std::this_thread::get_id() 에 해당
+//   long thread_id = (long int)pthread_self();
+//   printf("process id is %d\n", getpid());
+//   printf("this is thread %ld\n", thread_id);
+//   return NULL;
+// }
+
+// 각 스레드에서 수행할 서로 다른 작업들을 함수로 구현
 void *foo(void *arg) {
-  // 현재 실행 중인 thread 의 식별자(스레드 ID) 반환
-  // -> std::thread 로 치면 std::this_thread::get_id() 에 해당
-  long thread_id = (long int)pthread_self();
-  printf("process id is %d\n", getpid());
-  printf("this is thread %ld\n", thread_id);
+  printf("foo executed\n");
+  return NULL;
+}
+
+void *bar(void *arg) {
+  printf("bar executed\n");
+  return NULL;
+}
+
+void *baz(void *arg) {
+  printf("baz executed\n");
   return NULL;
 }
 
@@ -24,8 +41,8 @@ int main() {
 
   // 동일한 작업을 수행하는 여러 스레드 생성 -> 멀티스레딩
   pthread_create(&thread1, NULL, foo, NULL);
-  pthread_create(&thread2, NULL, foo, NULL);
-  pthread_create(&thread3, NULL, foo, NULL);
+  pthread_create(&thread2, NULL, bar, NULL);
+  pthread_create(&thread3, NULL, baz, NULL);
 
   // 생성한 스레드가 작업을 완료해서 Main thread 에 복귀할 때까지 대기
   pthread_join(thread1, NULL);
